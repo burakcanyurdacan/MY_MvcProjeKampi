63c1719307bd0a5081359af0add6843a0296162e
burakcanyurdacan <burakcanyurdacan@outlook.com>
2023-09-15 11:15:18 +0300

    Ders 4 ve Ders 5 Eksik kalan entity sınıfları oluşturuldu.

---
+++ EL/Concrate/About.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EL.Concrate
+{
+    public class About
+    {
+        public int AboutId { get; set; }
+        public string AboutDetails1 { get; set; }
+        public string AboutDetails2 { get; set; }
+        public string AboutImg1 { get; set; }
+        public string AboutImg2 { get; set; }
+    }
+}
