ff49f25b1145563f511e9af11f3e005ec2cdca28
Burak Can Yurdacan <burakcanyurdacan@outlook.com>
2023-10-10 23:25:07 +0300

    Ders 18,19 DAL katmanında Generic Repository ve BLL katmanında CategoryManager sınıfları eklendi.
    Ders 20 başlandı, UI katmanında Controller eklendi.

---
+++ BLL/Concrete/CategoryManager.cs
@@ -0,0 +1,32 @@
+﻿using DAL.Concrete.Repositories;
+using EL.Concrate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Concrete
+{
+    public class CategoryManager
+    {
+        GenericRepository<Category> repo = new GenericRepository<Category>();
+
+        public List<Category> GetListBLL()
+        {
+            return repo.GetAll();
+        }
+
+        public void AddCategoryBLL(Category p)
+        {
+            if (p.CategoryName == "")
+            {
+                //Hata mesajı verilecek
+            }
+            else
+            {
+                repo.Insert(p);
+            }
+        }
+    }
+}
---
+++ DAL/Concrete/Repositories/GenericRepository.cs
@@ -0,0 +1,48 @@
+﻿using DAL.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Concrete.Repositories
+{
+    public class GenericRepository<T> : IRepository<T> where T : class
+    {
+        Context c = new Context();
+        DbSet<T> obj;
+        public GenericRepository()
+        {
+            obj = c.Set<T>();
+        }
+
+        public void Delete(T item)
+        {
+            obj.Remove(item);
+            c.SaveChanges();
+        }
+
+        public List<T> GetAll()
+        {
+            return obj.ToList();
+        }
+
+        public List<T> GetAllFilter(Expression<Func<T, bool>> filter)
+        {
+            return obj.Where(filter).ToList();
+        }
+
+        public void Insert(T item)
+        {
+            obj.Add(item);
+            c.SaveChanges();
+        }
+
+        public void Update(T item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
